d7a7e47651a5631f6fc8c69c4005dea93e5fbd12
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-09 17:29:13 +0530

    packages added

--- BackEnd/src/main/java/lk/ijse/spring/pos/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/pos/config/WebAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "lk.ijse.spring.pos.controller")
+@ComponentScan(basePackages = {"lk.ijse.spring.pos.controller","lk.ijse.spring.pos.adviser"})
 public class WebAppConfig {
     public WebAppConfig(){
         System.out.println("WebAppConfig");

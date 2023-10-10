c02b74b719871d9b5d9795ac277a9f16531e2009
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-10 18:24:51 +0530

    PurchaseOrder updated

--- BackEnd/src/main/java/lk/ijse/spring/pos/service/impl/PurchaseOrderServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/pos/service/impl/PurchaseOrderServiceImpl.java
@@ -1,7 +1,11 @@
 package lk.ijse.spring.pos.service.impl;
 
+import lk.ijse.spring.pos.dto.OrderDetailsDTO;
 import lk.ijse.spring.pos.dto.OrdersDTO;
+import lk.ijse.spring.pos.entity.Item;
+import lk.ijse.spring.pos.entity.OrderDetails;
 import lk.ijse.spring.pos.entity.Orders;
+import lk.ijse.spring.pos.repo.ItemRepo;
 import lk.ijse.spring.pos.repo.OrderDetailsRepo;
 import lk.ijse.spring.pos.repo.OrdersRepo;
 import lk.ijse.spring.pos.service.PurchaseOrderService;
@@ -10,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+
 
 @Service
 @Transactional
@@ -22,14 +28,38 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
     private OrderDetailsRepo orderDetailsRepo;
 
     @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
     private ModelMapper mapper;
 
     @Override
     public void purchaseOrder(OrdersDTO dto) {
         if (ordersRepo.existsById(dto.getOid())) throw new RuntimeException("exist order id");
-        System.out.println(dto);
-        Orders map = mapper.map(dto, Orders.class);
-        System.out.println(map);
-        ordersRepo.save(map);
+
+        // Orders table & OrderDetails table
+        ordersRepo.save(mapper.map(dto, Orders.class));
+
+        // Update item qty
+        List<OrderDetailsDTO> orderDetails = dto.getOrderDetails();
+        for (OrderDetailsDTO detailsDTO : orderDetails) {
+            String itemCode = detailsDTO.getItemCode();
+
+            // get current item qty
+            Item item = itemRepo.findById(itemCode).orElseThrow(() -> new RuntimeException("Item not found with code: " + itemCode));
+            int qtyOnHand = item.getQtyOnHand();
+
+            // update qty
+            int updatedQty = qtyOnHand - detailsDTO.getQty();
+
+            // check if not updatedQty is - value
+            if (updatedQty >= 0) {
+                item.setQtyOnHand(updatedQty);
+                itemRepo.save(item);
+            } else {
+                throw new RuntimeException("Wrong item count in " + itemCode);
+            }
+
+        }
     }
 }
